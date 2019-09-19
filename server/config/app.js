ddffc9ba80d0944e5f542801436c54ba20459258
Srijanshakya <srijan.shakya@drivingsales.com>
2019-09-19 13:25:12 +0545

    Updated for heroku

--- server/config/app.js
+++ server/config/app.js
@@ -1,67 +1,74 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const expressValidator = require('express-validator');
-const log = require('morgan')
-const path = require('path')
-const cors = require('cors')
-const seed = require('./seed');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const expressValidator = require("express-validator");
+const log = require("morgan");
+const path = require("path");
+const cors = require("cors");
+const seed = require("./seed");
 
-module.exports = function () {
-    let server = express(),
-        create,
-        start;
+module.exports = function() {
+  let server = express(),
+    create,
+    start;
 
-    create = (config, db) => {
-        let routes = require('../routes');
-        // set all the server things
-        server.set('env', config.env);
-        server.set('port', config.port);
-        server.set('hostname', config.hostname);
-        
-        server.use(function(req, res, next) { //allow cross origin requests
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, token");
-            res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, PUT, DELETE, OPTIONS");
-            res.header("Access-Control-Allow-Headers","*")
-            next();
-        });
-        // add middleware to parse the json
-        server.use(bodyParser.json());
-        server.use(expressValidator())
-        server.use(bodyParser.urlencoded({
-            extended: false
-        }));
-        server.use('/uploads', express.static(path.join('uploads')))
-        server.use(log('combined'));
+  create = (config, db) => {
+    let routes = require("../routes");
+    // set all the server things
+    server.set("env", config.env);
+    server.set("port", config.port);
+    server.set("hostname", config.hostname);
 
-        //connect the database
-        mongoose.connect( process.env.MONGODB_URI || db.database,
-            { 
-                useNewUrlParser: true,
-                useCreateIndex: true
-            }
-        );
+    server.use(function(req, res, next) {
+      //allow cross origin requests
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, token"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+      );
+      res.header("Access-Control-Allow-Headers", "*");
+      next();
+    });
+    // add middleware to parse the json
+    server.use(bodyParser.json());
+    server.use(expressValidator());
+    server.use(
+      bodyParser.urlencoded({
+        extended: false
+      })
+    );
+    server.use("/uploads", express.static(path.join("uploads")));
+    server.use(log("combined"));
 
-        seed.seed();
+    //connect the database
+    mongoose.connect(process.env.MONGODB_URI || db.database, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
 
-        // Create link to Angular build directory
-        var distDir = __dirname + "/dist/";
-        server.use(express.static(distDir));
+    seed.seed();
 
-        // Set up routes
-        routes.init(server);
-    };
+    // Set up routes
+    routes.init(server);
+  };
 
-    
-    start = () => {
-        var servers = server.listen(process.env.PORT || 8080, function () {
-            var port = servers.address().port;
-            console.log("App now running on port", port);
-        });
-    };
-    return {
-        create: create,
-        start: start
-    };
+  start = () => {
+    var servers = server.listen(process.env.PORT || 8080, function() {
+      var port = servers.address().port;
+      console.log("App now running on port", port);
+    });
+
+    // Create link to Angular build directory
+    var distDir = __dirname + "/dist/";
+    console.log(distDir)
+    server.use(express.static(distDir));
+  };
+  return {
+    create: create,
+    start: start
+  };
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  userUrl: ' https://afternoon-mesa-10488.herokuapp.com:5000/api/v1'
+  userUrl: '/api/v1'
 };
